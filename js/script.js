da4de9f4d80592e4a155b944e627db389e5d3556
zuyoe <9xtwo5@naver.com>
2023-03-14 20:04:55 +0900

    기본 기능 완료

--- js/script.js
+++ js/script.js
@@ -1,4 +1,66 @@
 window.onload = function () {
+    // 모달창
+    let body = document.querySelector("body");
+    let modal = document.querySelector(".modal");
+    modal.addEventListener("click", function () {
+        modal.style.display = "none";
+        body.classList.add("active");
+    });
+    // 스크롤 기능
+    // 스크롤바의 상단위치
+    let scy = 0;
+    let scActive = 50;
+    scy = window.document.documentElement.scrollTop;
+
+    let header = document.querySelector(".header");
+    let logoW = document.querySelector(".logo-w");
+    let logoG = document.querySelector(".logo-g");
+
+    header.addEventListener("mouseenter", function () {
+        header.classList.add("header-active");
+        logoW.style.display = "none";
+        logoG.style.display = "block";
+    });
+    header.addEventListener("mouseleave", function () {
+        if (scy < scActive) {
+            header.classList.remove("header-active");
+            logoW.style.display = "block";
+            logoG.style.display = "none";
+        }
+    });
+
+    // 새로고침 시
+    if (scy > scActive) {
+        header.classList.add("header-active");
+        logoW.style.display = "none";
+        logoG.style.display = "block";
+    }
+
+    window.addEventListener("scroll", function () {
+        scy = window.document.documentElement.scrollTop;
+        // console.log("스크롤 : " + scy);
+        if (scy > scActive) {
+            header.classList.add("header-active");
+            logoW.style.display = "none";
+            logoG.style.display = "block";
+        } else {
+            header.classList.remove("header-active");
+            logoW.style.display = "block";
+            logoG.style.display = "none";
+        }
+    });
+
+    // 펼침 언어 기능
+    const langWord = document.querySelector(".language-word");
+    const language = document.querySelector(".language");
+    const languageLi = document.querySelector(".language li");
+    setTimeout(function () {
+        languageLi.style.transition = "all 0.2s";
+    }, 500);
+
+    langWord.addEventListener("click", function () {
+        language.classList.toggle("language-box-active");
+    });
     // 메뉴기능
     const nav = document.querySelector(".nav");
     const btMenu = document.querySelector(".bt-menu");
@@ -51,7 +113,13 @@ window.onload = function () {
         // 다음 비디오 재생
         // 처음으로 비디오 플레이헤드 이동
         videos[videoIndex].currentTime = 0;
-        videos[videoIndex].play();
+
+        // https://solbel.tistory.com/1912
+        // videos[videoIndex].play();
+        const playPromise = videos[videoIndex].play();
+        if (playPromise !== undefined) {
+            playPromise.then((_) => {}).catch((error) => {});
+        }
 
         // 방어코드: 다음주 추가 설명
         clearInterval(videoTimer);
@@ -120,7 +188,7 @@ window.onload = function () {
         });
     });
 
-    // 비지니스 슬라이드
+    // 비즈니스 슬라이드
     const swBusiness = new Swiper(".swBusiness", {
         loop: true,
         speed: 500,
